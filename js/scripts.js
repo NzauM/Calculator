a59a64dfea7caf59873810e7c39a0560e0538cdc
NzauM <mercywaenu16@gmail.com>
2019-09-17 11:44:51 +0300

    Add functions to substract and multiply

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,26 @@
 var add = function(number1,number2){
     return number1+number2;
     };
+    alert("ADDITION");
     var number1 = parseInt(prompt("Enter any number:"));
     var number2 = parseInt(prompt("Enter another number:"));
     var result = add(number1,number2);
     alert(number1 + "+" + number2 + "=" + result);
+
+var subtract = function(x,y){
+    return x-y;
+};
+alert("SUBSTRACTION");
+var x = parseInt(prompt("Enter any number:"));
+var y = parseInt(prompt("Enter another number:"));
+var result = subtract(x,y);
+alert(x + "-" + y + "=" +result);
+
+var multiply = function(a,b){
+    return a*b;
+};
+alert("MULTIPLICATION")
+var a = parseInt(prompt("Enter any number:"));
+var b = parseInt(prompt("Enter another number:"));
+var result=multiply(a,b);
+alert(a + "*" + b + "=" + result ); 
