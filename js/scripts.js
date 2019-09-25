10d2fe8cbbc11be6661fc21f09372ffcb9ccd230
NzauM <mercywaenu16@gmail.com>
2019-09-25 18:01:42 +0300

    Add a user interface for add and subtract functions using jQuery.

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,39 @@
-var add = function(number1,number2){
-    return number1+number2;
-    };
-    alert("ADDITION");
-    var number1 = parseInt(prompt("Enter any number:"));
-    var number2 = parseInt(prompt("Enter another number:"));
-    var result = add(number1,number2);
-    alert(number1 + "+" + number2 + "=" + result);
-
-var subtract = function(x,y){
-    return x-y;
+var add = function(number1, number2) {
+  return number1 + number2;
 };
-alert("SUBSTRACTION");
-var x = parseInt(prompt("Enter any number:"));
-var y = parseInt(prompt("Enter another number:"));
-var result = subtract(x,y);
-alert(x + "-" + y + "=" +result);
 
-var multiply = function(a,b){
-    return a*b;
+var subtract = function(x, y) {
+  return x - y;
 };
-alert("MULTIPLICATION")
-var a = parseInt(prompt("Enter any number:"));
-var b = parseInt(prompt("Enter another number:"));
-var result=multiply(a,b);
-alert(a + "*" + b + "=" + result ); 
+
+var multiply = function(a, b) {
+  return a * b;
+};
+
+$(document).ready(function() {
+  $("form").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#add1").val());
+    var number2 = parseInt($("#add2").val());
+    var result=(add(number1, number2));
+    $("#output").text(result);
+
+    var number1 = parseInt($("#minus1").val());
+    var number2 = parseInt($("#minus2").val());
+    var result = (subtract(number1,number2));
+    $("#substraction").text(result);
+  });
+});
+
+// $("#add1").val();
+// $("#add2").val();
+
+// var x = parseInt(prompt("Enter any number:"));
+// var y = parseInt(prompt("Enter another number:"));
+// var result = subtract(x, y);
+// alert(x + "-" + y + "=" + result);
+
+// var a = parseInt(prompt("Enter any number:"));
+// var b = parseInt(prompt("Enter another number:"));
+// var result = multiply(a, b);
+// alert(a + "*" + b + "=" + result);
